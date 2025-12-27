3f79a90493b6b5d8361540ddf34832d9b61c33a9
demiladeolawoye <78360945+demiladeolawoye@users.noreply.github.com>
2025-12-27 20:20:50 +0000

    Update forgot-pin.js

--- assets/js/auth/forgot-pin.js
+++ assets/js/auth/forgot-pin.js
@@ -1,9 +1,4 @@
-document.getElementById("resetBtn").onclick = () => {
-  const msg = document.getElementById("message");
-  msg.textContent = "Reset link sent. Continue to create new PIN.";
-  msg.className = "message success";
-
-  setTimeout(() => {
-    window.location.href = "create-new-pin.html";
-  }, 1200);
+document.getElementById("forgotForm").onsubmit = e => {
+  e.preventDefault();
+  location.href = "create-new-pin.html";
 };
