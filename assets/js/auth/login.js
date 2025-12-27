66ffc5719ee0b098cbbcb3df5de7e04815c1afba
demiladeolawoye <78360945+demiladeolawoye@users.noreply.github.com>
2025-12-27 20:20:04 +0000

    Update login.js

--- assets/js/auth/login.js
+++ assets/js/auth/login.js
@@ -1,18 +1,4 @@
-document.getElementById("loginBtn").onclick = () => {
-  const email = document.getElementById("email").value;
-  const pin = document.getElementById("pin").value;
-  const msg = document.getElementById("message");
-
-  if (!email || !pin) {
-    msg.textContent = "Please enter email and PIN";
-    msg.className = "message error";
-    return;
-  }
-
-  msg.textContent = "Login successful. Redirecting…";
-  msg.className = "message success";
-
-  setTimeout(() => {
-    window.location.href = "index.html";
-  }, 1000);
+document.getElementById("loginForm").onsubmit = e => {
+  e.preventDefault();
+  location.href = "index.html";
 };
