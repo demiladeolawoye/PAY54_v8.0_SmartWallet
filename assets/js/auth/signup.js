ae3a354ba66d15a1a7d50ee435a7a2964dd89a65
demiladeolawoye <78360945+demiladeolawoye@users.noreply.github.com>
2026-03-11 14:43:40 +0000

    Update signup.js

--- assets/js/auth/signup.js
+++ assets/js/auth/signup.js
@@ -1,4 +1,15 @@
 document.getElementById("signupForm").onsubmit = e => {
+
   e.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+
+  // Save to browser storage
+  localStorage.setItem("pay54_name", name);
+  localStorage.setItem("pay54_email", email);
+
+  // Continue to OTP page
   location.href = "otp.html";
+
 };
