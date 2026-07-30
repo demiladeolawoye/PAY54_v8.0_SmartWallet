1849d381c9a9c308b58e3f9884d9d97a8a2e2c06
demiladeolawoye <78360945+demiladeolawoye@users.noreply.github.com>
2026-07-30 10:23:19 +0100

    Update transactions.js

--- assets/js/transactions.js
+++ assets/js/transactions.js
@@ -1503,11 +1503,6 @@ const TX_REGISTRY = new Map();
 
 const TX_SETTLEMENTS = [];
 /* =========================
-   SETTLEMENT REGISTRY
-========================= */
-
-const TX_SETTLEMENTS = [];
-/* =========================
    CLEARING REGISTRY
 ========================= */
 
