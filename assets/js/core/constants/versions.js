983ad8868a3afe90614c0093dc89cc1fc89aaacd
demiladeolawoye <78360945+demiladeolawoye@users.noreply.github.com>
2026-09-23 11:02:10 +0100

    Update versions.js

--- assets/js/core/constants/versions.js
+++ assets/js/core/constants/versions.js
@@ -205,7 +205,10 @@
             CONTACTS:
                 "1.0.0",
 
-            BENEFICIARIES:
+                       BENEFICIARIES:
+                "1.0.0",
+
+            FUNDING:
                 "1.0.0"
 
         },
